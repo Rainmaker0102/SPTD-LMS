ceb4138aface31f8ec115bd501f8e643a38bbdbf
layoung3 <94134691+layoung3@users.noreply.github.com>
2023-03-16 09:01:09 -0400

    Add files via upload

--- library_management_system/lib/main.dart
+++ library_management_system/lib/main.dart
@@ -2,6 +2,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:library_management_system/authorization_control.dart';
+import 'package:library_management_system/loan_page.dart';
 import 'package:library_management_system/login_page.dart';
 import 'package:library_management_system/signup_page.dart';
 import 'package:library_management_system/splash_screen.dart';
@@ -29,7 +30,7 @@ class MyApp extends StatelessWidget {
 
         primarySwatch: Colors.red,
       ),
-      home: SplashScreen(),
+      home: LoanPage(),
     );
   }
 }
